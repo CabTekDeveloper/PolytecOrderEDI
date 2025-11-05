29878f3c189baea153d7d7b2bc8e5556d5b5125f
Wangchuk <chukong5@gmail.com>
2025-11-05 15:23:24 +1100

    Added code in CustomDrillingOnGenericPiece class to add hinges to generic pieces.

--- CabTek/PolytecConfiguredOrder/CustomDrillingOnGenericPiece.cs
+++ CabTek/PolytecConfiguredOrder/CustomDrillingOnGenericPiece.cs
@@ -9,7 +9,10 @@ namespace PolytecOrderEDI
         private static GenericPiece? ConfiguredPiece { get; set; }
         private static VinylPart? vinylPart { get; set; } = null;
         private static CabinetPart? cabPart { get; set; } = null;
-        private static BuildParameter_DrawerFront DrawerFrontParams { get; set; } = new();
+
+        private static BuildParameter_Door? DoorParams { get; set; } = null;
+        private static BuildParameter_DrawerFront? DrawerFrontParams { get; set; } = null;
+        private static BuildParameter_Handle? HandleParams { get; set; } = null;
 
         private static PRODUCTTYPE ProductType { get; set; } = PRODUCTTYPE.None;
 
@@ -18,6 +21,23 @@ namespace PolytecOrderEDI
         private static double Height { get; set; }
         private static double Width { get; set; }
         private static double Thickness { get; set; }
+
+        private static HINGETYPE HingeType { get; set; } = HINGETYPE.None;
+        private static double HingeCupInset { get; set; }
+        private static double HingeBlockInset { get; set; }
+        private static double HingeBlockHDIA { get; set; }
+        private static double HingeBlockHoleDepth { get; set; }
+
+        private static double BifoldHingeCupInset { get; set; }
+        private static double HTOD { get; set; }
+        private static double Hole1FromBot { get; set; }
+        private static double Hole2FromTop { get; set; }
+        private static double Hole3FromTop { get; set; }
+        private static double Hole4FromTop { get; set; }
+        private static double Hole5FromTop { get; set; }
+        private static double Hole6FromTop { get; set; }
+        private static int NumHoles { get; set; }
+
         private static double LINS { get; set; }
         private static double RINS { get; set; }
         private static int DTYP1 { get; set; }
@@ -44,7 +64,30 @@ namespace PolytecOrderEDI
             Width       = vinylPart != null ? vinylPart.Width       : cabPart != null ? cabPart.Width       : 0;
             Thickness   = vinylPart != null ? vinylPart.Thickness   : cabPart != null ? cabPart.Thickness   : 0;
 
-            if (cabPart != null) DrawerFrontParams = new(cabPart);
+            //if (cabPart != null) DrawerFrontParams = new(cabPart);
+
+            if (cabPart != null)
+            {
+                DoorParams = new(cabPart);
+                DrawerFrontParams = new(cabPart);
+                HandleParams = new(cabPart);
+            }
+
+            HingeType = vinylPart != null ? vinylPart.HingeType : DoorParams != null ? DoorParams.HingeType : HINGETYPE.None;
+            HingeCupInset = vinylPart != null ? vinylPart.HingeCupInset : DoorParams != null ? DoorParams.HingeCupInset : 0;
+            HingeBlockInset = vinylPart != null ? vinylPart.HingeBlockInset : DoorParams != null ? DoorParams.HingeBlockInset : 0;
+            HingeBlockHDIA = vinylPart != null ? vinylPart.HingeBlockHDIA : 0;
+            HingeBlockHoleDepth = vinylPart != null ? vinylPart.HingeBlockHoleDepth : 0;
+            BifoldHingeCupInset = vinylPart != null ? vinylPart.BifoldHingeCupInset : DoorParams != null ? DoorParams.BifoldHingeCupInset : 0;
+            HTOD = vinylPart != null ? vinylPart.HTOD : DoorParams != null ? DoorParams.HTOD : 0;
+
+            Hole1FromBot = vinylPart != null ? vinylPart.Hole1FromBot : DoorParams != null ? DoorParams.Hole1FromBot : 0;
+            Hole2FromTop = vinylPart != null ? vinylPart.Hole2FromTop : DoorParams != null ? DoorParams.Hole2FromTop : 0;
+            Hole3FromTop = vinylPart != null ? vinylPart.Hole3FromTop : DoorParams != null ? DoorParams.Hole3FromTop : 0;
+            Hole4FromTop = vinylPart != null ? vinylPart.Hole4FromTop : DoorParams != null ? DoorParams.Hole4FromTop : 0;
+            Hole5FromTop = vinylPart != null ? vinylPart.Hole5FromTop : DoorParams != null ? DoorParams.Hole5FromTop : 0;
+            Hole6FromTop = vinylPart != null ? vinylPart.Hole6FromTop : DoorParams != null ? DoorParams.Hole6FromTop : 0;
+            NumHoles = vinylPart != null ? vinylPart.NumHoles : DoorParams != null ? DoorParams.NumHoles : 0;
 
             LINS        = vinylPart != null ? vinylPart.RINS        : DrawerFrontParams != null ? DrawerFrontParams.RINS    : 0;     // The LINS of the Back view is the RINS of the Front view
             RINS        = vinylPart != null ? vinylPart.LINS        : DrawerFrontParams != null ? DrawerFrontParams.LINS    : 0;      // The RINS of the Back view is the LINS of the Front view 
@@ -64,7 +107,7 @@ namespace PolytecOrderEDI
             HasCustomHole1Drilling  = vinylPart != null ? (vinylPart.CustomHole1LeftInset > 0 && vinylPart.CustomHole1TopInset > 0 && vinylPart.CustomHole1HDIA > 0 && vinylPart.CustomHole1Depth > 0 && vinylPart.CustomHole1ApplyTarget != APPLYTARGET.None) : false;
         }
 
-        public static void AddDrillings( GenericPiece configuredPiece, VinylPart? vinyl_part = null, CabinetPart? cabinet_part = null)
+        public static void AddDrillings(GenericPiece configuredPiece, VinylPart? vinyl_part = null, CabinetPart? cabinet_part = null)
         {
             if (vinyl_part == null && cabinet_part == null) return;
 
@@ -80,9 +123,23 @@ namespace PolytecOrderEDI
             {
                 AddCustomHole1FromTopLeft();
             }
-            AddLeftAndRightVerticalHoles(DTYP1, INUP1);
-            AddLeftAndRightVerticalHoles(DTYP2, INUP2);
-            AddSingleSpotHole(addToSide: PartName.ToString().ToLower());   
+
+            if (HingeType == HINGETYPE.BlumLdf)
+            {
+                HingeType = HINGETYPE.Blum; //Set the  hinge type to Blum
+                AddHinges("right", HingeCupInset);
+            }
+            else if (HingeType == HINGETYPE.BlumRdf)
+            {
+                HingeType = HINGETYPE.Blum; //Set the  hinge type to Blum
+                AddHinges("left", HingeCupInset);
+            }
+            else
+            {
+                AddLeftAndRightVerticalHoles(DTYP1, INUP1);
+                AddLeftAndRightVerticalHoles(DTYP2, INUP2);
+                AddSingleSpotHole(addToSide: PartName.ToString().ToLower());
+            }
         }
 
         //Method to add vertical holes on left and right on a panel
@@ -163,6 +220,45 @@ namespace PolytecOrderEDI
                 var applyTarget = CustomHole1ApplyTarget == APPLYTARGET.Front ? ApplyTarget.Front : ApplyTarget.Back;
                 var leftInset = CustomHole1ApplyTarget == APPLYTARGET.Front ? CustomHole1LeftInset : Width - CustomHole1LeftInset;
                 ConfiguredPiece.Features.AddHoleFromTopLeft(applyTarget, CustomHole1TopInset, leftInset, CustomHole1HDIA / 2, CustomHole1Depth);
+            }
+        }
+
+
+
+        //AddDrillings hinge holes to door from back
+        private static void AddHinges(string addToSide, double offset)
+        {
+            if (ConfiguredPiece != null)
+            {
+                //if (NumHoles > 0 && offset > 0 && HingeType != HINGETYPE.None && HingeType != HINGETYPE.Blum11)
+                if (NumHoles > 0 && offset > 0 && (HingeType == HINGETYPE.Blum || HingeType == HINGETYPE.Hettich))
+                {
+                    var Htype = HingeType == HINGETYPE.Blum ? BorgEdi.Enums.HingeType.Blum : BorgEdi.Enums.HingeType.Hettich;
+
+                    if (addToSide == "left" || addToSide == "right")
+                    {
+                        var primaryAxisReference = (addToSide == "left") ? PrimaryAxisReference.Left : PrimaryAxisReference.Right;
+
+                        if (Hole1FromBot > 0) ConfiguredPiece.Features.Add(new Hinge(Htype, primaryAxisReference, offset, AdjacentAxisReference.Bottom, Hole1FromBot));
+                        if (Hole2FromTop > 0) ConfiguredPiece.Features.Add(new Hinge(Htype, primaryAxisReference, offset, AdjacentAxisReference.Top, Hole2FromTop));
+                        if (Hole3FromTop > 0) ConfiguredPiece.Features.Add(new Hinge(Htype, primaryAxisReference, offset, AdjacentAxisReference.Top, Hole3FromTop));
+                        if (Hole4FromTop > 0) ConfiguredPiece.Features.Add(new Hinge(Htype, primaryAxisReference, offset, AdjacentAxisReference.Top, Hole4FromTop));
+                        if (Hole5FromTop > 0) ConfiguredPiece.Features.Add(new Hinge(Htype, primaryAxisReference, offset, AdjacentAxisReference.Top, Hole5FromTop));
+                        if (Hole6FromTop > 0) ConfiguredPiece.Features.Add(new Hinge(Htype, primaryAxisReference, offset, AdjacentAxisReference.Top, Hole6FromTop));
+                    }
+                    else if (addToSide == "top" || addToSide == "bottom")
+                    {
+                        var primaryAxisReference = (addToSide == "top") ? PrimaryAxisReference.Top : PrimaryAxisReference.Bottom;
+
+                        if (Hole1FromBot > 0) ConfiguredPiece.Features.Add(new Hinge(Htype, primaryAxisReference, offset, AdjacentAxisReference.Right, Hole1FromBot));
+                        if (Hole2FromTop > 0) ConfiguredPiece.Features.Add(new Hinge(Htype, primaryAxisReference, offset, AdjacentAxisReference.Left, Hole2FromTop));
+                        if (Hole3FromTop > 0) ConfiguredPiece.Features.Add(new Hinge(Htype, primaryAxisReference, offset, AdjacentAxisReference.Left, Hole3FromTop));
+                        if (Hole4FromTop > 0) ConfiguredPiece.Features.Add(new Hinge(Htype, primaryAxisReference, offset, AdjacentAxisReference.Left, Hole4FromTop));
+                        if (Hole5FromTop > 0) ConfiguredPiece.Features.Add(new Hinge(Htype, primaryAxisReference, offset, AdjacentAxisReference.Left, Hole5FromTop));
+                        if (Hole6FromTop > 0) ConfiguredPiece.Features.Add(new Hinge(Htype, primaryAxisReference, offset, AdjacentAxisReference.Left, Hole6FromTop));
+                    }
+                }
+
             }
         }
     }
